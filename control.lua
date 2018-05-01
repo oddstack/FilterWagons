8ba370f102e36e53fe0e4e728a2249e3af3e6943
Robert Schneider <oddstack@gmail.com>
2018-05-01 18:37:56 +0200

    working filter test

--- control.lua
+++ control.lua
@@ -2,11 +2,30 @@ script.on_event({defines.events.on_tick},
    function (e)
       if e.tick % 60 == 0 then --common trick to reduce how often this runs, we don't want it running every tick, just 1/second
          for key, entity in pairs (game.surfaces[1].find_entities_filtered{name = "cargo-wagon"} ) do
-			print("1")
+			if entity.train then
+				update_train(entity.train, entity)
+			end
 		 end
       end
    end
 )
+
+function update_train(train, wagon)
+	if (train.state == defines.train_state.wait_station) then
+		wagon.set_filter(1, "iron-plate")
+		inv = wagon.get_inventory(defines.inventory.cargo_wagon)
+		
+		inv.set_filter(1, "iron-plate")
+		
+		
+		canSup = inv.supports_filters()
+		if (canSup) then
+			print("waiting true " .. inv.get_filter(1))
+		else 
+			print("waiting false")
+		end
+	end
+end
 
 function print(s)
 	for index,player in pairs(game.connected_players) do  --loop through all online players on the server
